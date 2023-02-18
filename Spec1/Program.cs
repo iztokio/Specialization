79dea288ea22c579d0a3ec55cdf1b8759a94dc5c
Artem <iztokio1984@mail.ru>
2023-02-18 22:38:28 +0300

    Spec_3commit

--- Spec1/Program.cs
+++ Spec1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the number of strings: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+string[] strings = new string[n];
+Console.WriteLine("Enter the strings: ");
+for (int i = 0; i < n; i++)
+{
+    strings[i] = Console.ReadLine();
+}
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newStrings = new string[count];
+int index = 0;
+for (int i = 0; i < n; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        newStrings[index] = strings[i];
+        index++;
+    }
+}
+Console.WriteLine("Strings with length less than or equal to 3 characters: ");
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine(newStrings[i]);
+}
